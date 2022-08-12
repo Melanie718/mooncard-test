cdd33286f877b17f8d22fce222b8427690f6ce7e
Melanie718 <melanievano@live.fr>
2022-08-12 17:01:05 +0200

    Added a searching bar

--- frontend-junior-developer/src/App.tsx
+++ frontend-junior-developer/src/App.tsx
@@ -1,19 +1,27 @@
 import useFetch, { ApiResponse } from "./API";
 import "./App.css";
 import Pokemon from "./Models/Pokemon";
-import { Card } from "react-bootstrap";
+import { Card, InputGroup, Form } from "react-bootstrap";
 import ScrollUp from "./components/ScrollUp";
 import { MdCatchingPokemon } from "react-icons/md";
 import ErrorAlert from "./components/ErrorAlert";
+import { useState } from "react";
 
 function App() {
   const pokemons: ApiResponse = useFetch(
-    "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
+    "https://pokeapi.co/api/v2/pokemon?limit=865&offset=0"
   );
+  const [inputValue, setInputValue] = useState("");
 
   return (
     <div className="App">
       <h1>Pokédex </h1>
+      <InputGroup id="search-bar">
+        <Form.Control
+          placeholder={"Looking for a specific pokemon?"}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+      </InputGroup>
       {!pokemons.loading && pokemons.error !== undefined && (
         <ErrorAlert content={pokemons.error} />
       )}
@@ -27,22 +35,26 @@ function App() {
         )}
         {!pokemons.loading &&
           pokemons.data &&
-          pokemons.data.results.map((pokemon: Pokemon) => (
-            <div className="pokemon-preview" key={pokemon.url.split("/")[6]}>
-              <Card>
-                <Card.Body>
-                  <Card.Title className="pokemon-title">
-                    #{pokemon.url.split("/")[6]} {pokemon.name}
-                  </Card.Title>
-                  <Card.Img
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                      pokemon.url.split("/")[6]
-                    }.png `}
-                  />
-                </Card.Body>
-              </Card>
-            </div>
-          ))}
+          pokemons.data.results
+            .filter((pokemon: Pokemon) =>
+              pokemon.name.includes(inputValue.toLowerCase())
+            )
+            .map((pokemon: Pokemon) => (
+              <div className="pokemon-preview" key={pokemon.url.split("/")[6]}>
+                <Card>
+                  <Card.Body>
+                    <Card.Title className="pokemon-title">
+                      #{pokemon.url.split("/")[6]} {pokemon.name}
+                    </Card.Title>
+                    <Card.Img
+                      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                        pokemon.url.split("/")[6]
+                      }.png `}
+                    />
+                  </Card.Body>
+                </Card>
+              </div>
+            ))}
       </div>
       <ScrollUp />
     </div>
